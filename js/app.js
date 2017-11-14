1209acd9cff957004ed790e5e442d1590622bc0b
akbaruddin <akbaruddin@live.in>
2017-11-14 21:53:33 +0530

    init codes

---
+++ js/app.js
@@ -0,0 +1,16 @@
+;(function($, App){
+  "use strict";
+
+  function timer() {
+    $("#timer").on("click", function () {
+      
+    });
+  }
+
+  function init() {
+    timer();
+  };
+
+  App.init = init;
+
+}(jQuery, (window.App = window.App || {} )));
