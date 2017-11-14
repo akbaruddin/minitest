b336041d20fbd2269a969a60d646096798d6b28e
akbaruddin <akbaruddin@live.in>
2017-11-14 22:57:13 +0530

    timer and timer functions

--- js/app.js
+++ js/app.js
@@ -2,9 +2,41 @@
   "use strict";
 
   function timer() {
+    var clock = {
+      totalSeconds: 0,
+      start: function () {
+        var _this = this;
+
+        this.interval = setInterval(function () {
+          _this.totalSeconds += 1;
+
+          var min = Math.floor(_this.totalSeconds / 60 % 60);
+          var sec = ("00" + parseInt(_this.totalSeconds % 60)).substr(-2)
+
+          $('#timer span').text(min + ":" + sec)
+        }, 1000);
+      },
+      pause: function () {
+        clearInterval(this.interval);
+        delete this.interval;
+      },
+      resume: function () {
+        if (!this.interval) this.start();
+      }
+    }
+
+    clock.start();
+    var isPause = false;
     $("#timer").on("click", function () {
-      
+        if (isPause) {
+          clock.resume();
+        } else {
+          clock.pause();
+        }
+        isPause = !isPause;
+        $(this).find('.icon').toggleClass('icon-pause icon-play');
     });
+
   }
 
   function init() {
